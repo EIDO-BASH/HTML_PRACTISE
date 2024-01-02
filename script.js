72279e590608891771348528467b0c6dd5557351
EIDO-BASH <onyebashiesther@gmail.com>
2024-01-02 10:28:56 -0800

    added a script tag

---
+++ script.js
@@ -0,0 +1,15 @@
+// var fruit = ["berries", "bannana", " melon"];
+// console.log(fruit);
+
+// console.log(fruit[0]);
+// let operator = "*";
+// let num1 = 50;
+// let num2 = 100;
+
+// if (operator == "+") console.log(num1 + num2);
+
+// if (operator == "-") console.log(num2 - num1);
+
+// if (operator == "*") console.log(num1 * num2);
+
+// if (operator == "/") console.log(num2 / num1);
